50be8cdf8beb5ccc0b4fcd5df5adc6d00e4f36e5
Pierre Billaud <45881846+PierreBio@users.noreply.github.com>
2024-02-25 10:33:31 +0100

    Random forest model added

--- src/models/model_factory.py
+++ src/models/model_factory.py
@@ -2,6 +2,7 @@ from src.models.gradient_boosting import GradientBoostingModel
 from src.models.logistic_regression import LogisticRegressionModel
 from src.models.naive_bayes import NaiveBayesModel
 from src.models.neural_network import NeuralNetworkModel
+from src.models.random_forest import RandomForestModel
 from src.models.svm import SVMModel
 
 class ModelFactory:
@@ -17,5 +18,7 @@ class ModelFactory:
             return NaiveBayesModel(X_train, Y_train, **config)
         elif model_type == 'gradient_boosting':
             return GradientBoostingModel(X_train, Y_train, **config)
+        elif model_type == 'random_forest':
+            return RandomForestModel(X_train, Y_train, **config)
         else:
             raise ValueError(f"Unknown model type: {model_type}")
